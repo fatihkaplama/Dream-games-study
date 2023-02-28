b8b2ddafa5237d04333f21510ac7014fdc05c947
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2023-02-28 21:51:50 +0300

    Create the repository class

---
+++ src/main/java/com/fatikaplama/dreamgames/study/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.fatikaplama.dreamgames.study.repository;
+
+import com.fatikaplama.dreamgames.study.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
